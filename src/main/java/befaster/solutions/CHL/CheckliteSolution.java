e4eb875bffb8a942646f7d9214338e09cb372654
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-11-09 17:25:22 +0000

    Sat Nov 09 17:25:22 GMT 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,24 +1,55 @@
 package befaster.solutions.CHL;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CheckliteSolution {
 
-    private HashMap<Character, Integer> skus = new HashMap<>();
+    private Map<Character, Product> products = new HashMap<>();
 
     public CheckliteSolution() {
-        skus.put('A', 50);
-        skus.put('B', 30);
+        products.put('A', new Product(50, 3, 130));
+        products.put('B', new Product(30, 2, 45));
+        products.put('C', new Product(20));
+        products.put('D', new Product(15));
     }
 
     public Integer checklite(String basket) {
         int total = 0;
+        Map<Character, Integer> productCounter = new HashMap<>();
 
         for (char sku : basket.toCharArray()) {
-            total += skus.get(sku);
+            productCounter.merge(sku, 1, Integer::sum);
+        }
+
+        for (char sku : productCounter.keySet()) {
+            total += products.get(sku).calculatePriceFor(productCounter.get(sku));
         }
 
         return total;
     }
+
+    private static class Product {
+        private Integer price;
+        private Integer multiBuyQuantity;
+        private Integer multiBuyPrice;
+
+        public Product(int price) {
+            this.price = price;
+            this.multiBuyQuantity = null;
+            this.multiBuyPrice = null;
+        }
+
+        public Product(int price, int multiBuyQuantity, int multiBuyPrice) {
+            this.price = price;
+            this.multiBuyQuantity = multiBuyQuantity;
+            this.multiBuyPrice = multiBuyPrice;
+        }
+
+        public Integer calculatePriceFor(Integer quantity) {
+            return price * quantity;
+        }
+    }
 }
+
 
