519b2bf502426d316657269a89a86e85515694af
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-11-09 17:04:22 +0000

    Sat Nov 09 17:04:22 GMT 2019

--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -1,7 +1,21 @@
 package befaster.solutions.CHL;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CheckliteSolutionTest {
+    private CheckliteSolution checkliteSolution;
 
+    @Before
+    public void setUp() throws Exception {
+        checkliteSolution = new CheckliteSolution();
+    }
+
+    @Test
+    public void givenOneAProductInBasket_thenPriceIs50() {
+        assertThat(checkliteSolution.checklite("A"), Matchers.is(50));
+    }
 }
