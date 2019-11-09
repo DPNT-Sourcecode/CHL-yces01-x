c50ec1e59e22ce2937222b949227f4595f2f987e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-11-09 17:13:22 +0000

    Sat Nov 09 17:13:22 GMT 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,7 +1,24 @@
 package befaster.solutions.CHL;
 
+import java.util.HashMap;
+
 public class CheckliteSolution {
-    public Integer checklite(String skus) {
-        return 50;
+
+    private HashMap<Character, Integer> skus = new HashMap<>();
+
+    public CheckliteSolution() {
+        skus.put('A', 50);
+        skus.put('B', 30);
+    }
+
+    public Integer checklite(String basket) {
+        int total = 0;
+
+        for (char sku : basket.toCharArray()) {
+            total += skus.get(sku);
+        }
+
+        return total;
     }
 }
+
